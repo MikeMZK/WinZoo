9072fef2f2c2a14db4fb1c5aab4eef46c0388d36
SteWar <Steffi.20@t-online.de>
2017-09-21 10:26:35 +0200

    fe

--- WinZoo/frmMain.Designer.cs
+++ WinZoo/frmMain.Designer.cs
@@ -28,20 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pb_Eingang = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_Eingang)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pb_Eingang
+            // 
+            this.pb_Eingang.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pb_Eingang.Location = new System.Drawing.Point(0, 0);
+            this.pb_Eingang.Name = "pb_Eingang";
+            this.pb_Eingang.Size = new System.Drawing.Size(435, 218);
+            this.pb_Eingang.TabIndex = 0;
+            this.pb_Eingang.TabStop = false;
             // 
             // frmZoo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(435, 218);
+            this.Controls.Add(this.pb_Eingang);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
             this.Name = "frmZoo";
             this.Text = "Zoo";
+            ((System.ComponentModel.ISupportInitialize)(this.pb_Eingang)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pb_Eingang;
     }
 }
 
