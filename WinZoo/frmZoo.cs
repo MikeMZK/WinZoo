d24cd286450c5607a3e947f1e843e7989a8a77e6
SteWar <Steffi.20@t-online.de>
2017-09-25 14:23:32 +0200

    Design

--- WinZoo/frmZoo.cs
+++ WinZoo/frmZoo.cs
@@ -12,7 +12,7 @@ namespace WinZoo
 {
     public partial class frmZoo : Form
     {
-        Image ClosedExitDoor, OpenedExitDoor, EnterZooShine;
+        Image ClosedExitDoor, OpenedExitDoor, EnterZooShine, BackgroundZoo, Affe, Elefant, Panda, Gras, Pfeil;
 
         public frmZoo()
         {
@@ -21,14 +21,30 @@ namespace WinZoo
             ClosedExitDoor = Image.FromFile("ClosedExitDoor.png");
             OpenedExitDoor = Image.FromFile("OpenedExitDoor.png");
             pb_Exit.Image = ClosedExitDoor;
-
+            BackgroundZoo = Image.FromFile("BackgroundZoo.jpg");
             EnterZooShine = Image.FromFile("EnterZooShine.png");
+            Affe = Image.FromFile("Affe.png");
+            Elefant = Image.FromFile("Elefant.png");
+            Panda = Image.FromFile("Panda.png");
+            Gras = Image.FromFile("Gras.jpg");
+            Pfeil = Image.FromFile("Pfeil.png");
             pb_Eingang.Image = null;
+
+            pb_Affen.Enabled = false;
+            pb_Panda.Enabled = false;
+            pb_Elefanten.Enabled = false;
+            pb_Pfeil.Enabled = false;
+            pb_Eingang.BringToFront();
         }
 
         private void pb_Eingang_Click(object sender, EventArgs e)
         {
-
+            ActiveForm.BackgroundImage = BackgroundZoo;
+            pb_Eingang.Enabled = false;
+            pb_Affen.Enabled =true;
+            pb_Panda.Enabled = true;
+            pb_Elefanten.Enabled =true;
+            pb_Panda.BringToFront();
         }
         private void pb_Eingang_MouseEnter(object sender, EventArgs e)
         {
@@ -37,6 +53,77 @@ namespace WinZoo
         private void pb_Eingang_MouseLeave(object sender, EventArgs e)
         {
             pb_Eingang.Image = null;
+        }
+
+        private void pb_Elefanten_Click(object sender, EventArgs e)
+        {
+            ActiveForm.BackgroundImage = Gras;
+            pb_Affen.Enabled = false;
+            pb_Panda.Enabled = false;
+            pb_Elefanten.Enabled = false;
+            pb_Pfeil.Enabled = true;
+            pb_Pfeil.Image = Pfeil;
+            pb_Pfeil.BringToFront();
+        }
+
+        private void pb_Elefanten_MouseEnter(object sender, EventArgs e)
+        {
+            pb_Elefanten.Image = Elefant;
+        }
+
+        private void pb_Elefanten_MouseLeave(object sender, EventArgs e)
+        {
+            pb_Elefanten.Image = null;
+        }
+
+        private void pb_Panda_Click(object sender, EventArgs e)
+        {
+            ActiveForm.BackgroundImage = Gras;
+            pb_Affen.Enabled = false;
+            pb_Panda.Enabled = false;
+            pb_Elefanten.Enabled = false;
+            pb_Pfeil.Enabled = true;
+            pb_Pfeil.Image = Pfeil;
+            pb_Pfeil.BringToFront();
+
+        }
+
+        private void pb_Pfeil_Click(object sender, EventArgs e)
+        {
+            ActiveForm.BackgroundImage = BackgroundZoo;
+            pb_Affen.Enabled = true;
+            pb_Panda.Enabled = true;
+            pb_Elefanten.Enabled = true;
+        }
+
+        private void pb_Panda_MouseEnter(object sender, EventArgs e)
+        {
+            pb_Panda.Image = Panda;
+        }
+
+        private void pb_Panda_MouseLeave(object sender, EventArgs e)
+        {
+            pb_Panda.Image = null;
+        }
+
+        private void pb_Affen_Click(object sender, EventArgs e)
+        {
+            ActiveForm.BackgroundImage = Gras;
+            pb_Affen.Enabled = false;
+            pb_Panda.Enabled = false;
+            pb_Elefanten.Enabled = false;
+            pb_Pfeil.Enabled = true;
+            pb_Pfeil.Image = Pfeil;
+        }
+
+        private void pb_Affen_MouseEnter(object sender, EventArgs e)
+        {
+            pb_Affen.Image = Affe;
+        }
+
+        private void pb_Affen_MouseLeave(object sender, EventArgs e)
+        {
+            pb_Affen.Image = null;
         }
 
         private void pb_Exit_Click(object sender, EventArgs e)
